114a9bac4b80e9b75c444ce2f036afdf8ee3735f
Joao Lopes <joao.lopes@gamesys.co.uk>
2017-08-03 12:14:23 +0100

    Added linting

--- src/index.js
+++ src/index.js
@@ -2,12 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './main.scss';
 
-class App extends React.Component {
-    render() {
-        return (
-            <div>Example</div>
-        );
-    }
-}
+const App = () => (
+    <div>Example</div>
+);
 
 ReactDOM.render(<App />, document.getElementById('root'));
