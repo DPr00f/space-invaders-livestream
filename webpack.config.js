8f625c2a97e8dcb40e7a4e03d3603ec7b7f20c5a
Joao Lopes <joao.lopes@gamesys.co.uk>
2017-08-03 10:05:45 +0100

    Added webpack config and babelrc

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const webpack = require('webpack');
+const path = require('path');
+
+const srcFolder = path.resolve('./src');
+const outputFolder = path.join(__dirname, 'output');
+const jsEntry = path.resolve(srcFolder, 'index.js');
+
+module.exports = {
+    devtool: 'source-map',
+    context: srcFolder,
+    entry: {
+        bundle: jsEntry
+    },
+    output: {
+        path: outputFolder,
+        filename: '[name].js'
+    },
+    module: {
+        loaders: [
+        {
+            test: /\.(jsx?)$/,
+            exclude: /node_modules/,
+            use: [
+                {
+                    loader: 'babel-loader',
+                    query: {
+                        cacheDirectory: true
+                    }
+                }
+            ]
+        }
+        ]
+    }
+};
