eb5f646dc7d1bb7c949f6bfb6d053f8aaf6f3efa
Joao Lopes <joao.lopes@gamesys.co.uk>
2017-08-03 11:59:35 +0100

    Added styles

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './main.scss';
 
 class App extends React.Component {
     render() {
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,13 @@
 const webpack = require('webpack');
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-const srcFolder = path.resolve('./src');
+const srcFolder = path.resolve(__dirname, 'src');
 const outputFolder = path.join(__dirname, 'output');
 const jsEntry = path.resolve(srcFolder, 'index.js');
+const stylesEntry = path.resolve(srcFolder, 'main.scss');
+
+const extractCSS = new ExtractTextPlugin({ filename: 'main.css' });
 
 module.exports = {
     devtool: 'source-map',
@@ -16,20 +20,33 @@ module.exports = {
         filename: '[name].js',
         publicPath: '/assets/'
     },
+    plugins: [
+        extractCSS
+    ],
     module: {
         loaders: [
-        {
-            test: /\.(jsx?)$/,
-            exclude: /node_modules/,
-            use: [
-                {
-                    loader: 'babel-loader',
-                    query: {
-                        cacheDirectory: true
+            {
+                test: /\.(jsx?)$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: 'babel-loader',
+                        query: {
+                            cacheDirectory: true
+                        }
                     }
-                }
-            ]
-        }
+                ]
+            },
+            {
+                test: /\.scss$/,
+                use: extractCSS.extract({
+                    fallback: 'style-loader',
+                    use: [
+                        'css-loader',
+                        'sass-loader'
+                    ]
+                })
+            }
         ]
     }
 };
